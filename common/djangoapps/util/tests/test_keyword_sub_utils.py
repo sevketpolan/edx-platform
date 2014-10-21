32450548d1a3afd0a1d436a11575f1cc88984e50
njdup <njdupp@gmail.com>
2014-10-20 23:40:34 -0700

    Updated tests to use ddt library

--- common/djangoapps/util/tests/test_keyword_sub_utils.py
+++ common/djangoapps/util/tests/test_keyword_sub_utils.py
@@ -6,12 +6,16 @@ from django.test import TestCase
 from student.tests.factories import UserFactory
 from xmodule.modulestore.tests.factories import CourseFactory
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
+from ddt import ddt, file_data
+from mock import Mock, patch
+
 
 from student.models import anonymous_id_for_user
 from util.date_utils import get_default_time_display
 from util import keyword_substitution as Ks
 
 
+@ddt
 class KeywordSubTest(ModuleStoreTestCase):
 
     def setUp(self):
@@ -50,21 +54,24 @@ class KeywordSubTest(ModuleStoreTestCase):
             '%%COURSE_END_DATE%%': course_end_date_sub
         }
 
-    def test_course_name_sub(self):
-        test_string = 'Course Display Name: %%COURSE_DISPLAY_NAME%%'
+    @file_data('data/test_keyword_coursename_sub.json')
+    def test_course_name_sub(self, test_info):
+        """ Tests subbing course name in various scenarios """
         course_name = self.course.display_name
-        result = Ks.substitute_keywords_with_data(test_string, self.user.id, self.course.id)
+        result = Ks.substitute_keywords_with_data(test_info['test_string'], self.user.id, self.course.id)
 
         self.assertIn(course_name, result)
-        self.assertNotIn('%%COURSE_DISPLAY_NAME%%', result)
+        self.assertEqual(result, test_info['expected'])
 
-    def test_anonymous_id_sub(self):
-        test_string = "This is the test string. sub this: %%USER_ID%% into anon_id"
-        anon_id = anonymous_id_for_user(self.user, None)
-        result = Ks.substitute_keywords_with_data(test_string, self.user.id, self.course.id)
+    @file_data('data/test_keyword_anonid_sub.json')
+    def test_anonymous_id_subs(self, test_info):
+        """ Tests subbing anon user id in various scenarios """
+        anon_id = '123456789'
+        with patch.dict(Ks.KEYWORD_FUNCTION_MAP, {'%%USER_ID%%': lambda x, y: anon_id}):
+            result = Ks.substitute_keywords_with_data(test_info['test_string'], self.user.id, self.course.id)
 
-        self.assertIn("this: " + anon_id + " into", result)
-        self.assertNotIn("%%USER_ID%%", result)
+            self.assertIn(anon_id, result)
+            self.assertEqual(result, test_info['expected'])
 
     def test_name_sub(self):
         test_string = "This is the test string. subthis:  %%USER_FULLNAME%% into user name"
@@ -73,16 +80,6 @@ class KeywordSubTest(ModuleStoreTestCase):
 
         self.assertNotIn('%%USER_FULLNAME%%', result)
         self.assertIn(user_name, result)
-
-    def test_anonymous_id_sub_html(self):
-        """
-        Test that sub-ing works in html tags as well
-        """
-        test_string = "<some tag>%%USER_ID%%</some tag>"
-        anon_id = anonymous_id_for_user(self.user, None)
-        result = Ks.substitute_keywords_with_data(test_string, self.user.id, self.course.id)
-
-        self.assertEquals(result, "<some tag>" + anon_id + "</some tag>")
 
     def test_illegal_subtag(self):
         """
@@ -102,37 +99,20 @@ class KeywordSubTest(ModuleStoreTestCase):
 
         self.assertEquals(test_string, result)
 
-    def test_sub_multiple_tags(self):
-        """
-        Test that sub-ing works with multiple subtags
-        """
-        test_string = 'the user with id %%USER_ID%% is named %%USER_FULLNAME%%'
-        anon_id = anonymous_id_for_user(self.user, None)
-        result = Ks.substitute_keywords_with_data(test_string, self.user.id, self.course.id)
+    @file_data('data/test_keywordsub_multiple_tags.json')
+    def test_sub_mutiltple_tags(self, test_info):
+        """ Test that subbing works with multiple subtags """
+        anon_id = '123456789'
+        patched_dict = {
+            '%%USER_ID%%': lambda x,y: anon_id,
+            '%%USER_FULLNAME%%': lambda x,y: self.user.profile.name,
+            '%%COURSE_DISPLAY_NAME': lambda x,y: self.course.display_name,
+            '%%COURSE_END_DATE': lambda x,y: get_default_time_display(self.course.end)
+        }
 
-        self.assertNotIn('%%USER_ID%%', result)
-        self.assertNotIn('%%USER_FULLNAME%%', result)
-
-        self.assertIn(anon_id, result)
-        self.assertIn(self.user.profile.name, result)
-
-    def test_sub_multiple_tags_with_invalid(self):
-        """
-        Test that sub-ing works with multiple tags, with invalid tags.
-
-        The invalid tag should simply be ignored
-        """
-        test_string = 'The user with id %%user-id%% is named %%USER_FULLNAME%% and is in %%COURSE_DISPLAY_NAME%%'
-        result = Ks.substitute_keywords_with_data(test_string, self.user.id, self.course.id)
-
-        # Check that valid tags properly replaced
-        self.assertNotIn('%%USER_FULLNAME%%', result)
-        self.assertNotIn('%%COURSE_DISPLAY_NAME%%', result)
-        self.assertIn(self.user.profile.name, result)
-        self.assertIn(self.course.display_name, result)
-
-        # Check that invalid tag is ignored
-        self.assertIn('%%user-id%%', result)
+        with patch.dict(Ks.KEYWORD_FUNCTION_MAP, patched_dict):
+            result = Ks.substitute_keywords_with_data(test_info['test_string'], self.user.id, self.course.id)
+            self.assertEqual(result, test_info['expected'])
 
     def test_no_subbing_empty_subtable(self):
         """
